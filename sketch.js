9d552e93b163bdaf3175050cd21bdf6c5a4b4104
shreyans1234432 <78852144+shreyans1234432@users.noreply.github.com>
2021-03-12 12:45:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var cat1 = createSprite(80,80,40,40);
+cat1.velocityX = 5;
+cat1.velocityY = -3;
+
+mouse.velocityX = 
+function preload() {
+    //load the images here
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
